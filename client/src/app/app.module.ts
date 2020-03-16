ce1518ccc13d8affffda405d3da113f5a18e7446
Mark Todd <marktodd.hl@gmail.com>
2020-03-16 20:48:51 +0100

    Add filterbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterbarComponent } from './components/filterbar/filterbar.component';
+import { JobListComponent } from './components/job-list/job-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { OfferListComponent } from './components/offer-list/offer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FilterbarComponent,
+    JobListComponent,
+    OfferListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatChipsModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: []
+  schemas: [],
 })
-export class AppModule { }
+export class AppModule {}
